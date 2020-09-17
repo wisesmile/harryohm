5d8dad8427570309ab73c58b57ebcb8879d0733b
mpaz <mpazaryna@gmail.com>
2020-09-17 15:22:21 -0400

    removed a hrefs

--- pages/about.js
+++ pages/about.js
@@ -13,52 +13,33 @@ export default function About() {
         <h1 className={styles.title}>About</h1>
 
         <div className={styles.grid}>
-          <a
-            href="https://yogawithpaz.com/"
-            className={styles.card}
-            target="_new"
-          >
-            <h3>About</h3>
-            <p>
-              In 1982, I started a formal practice of meditation, a study of
-              Vedanta Philosophy and a Hatha Yoga practice. I taught my first
-              Hatha Yoga class in 1985 at Seton Hall University.
-            </p>
-          </a>
+          <h3>The Early Days</h3>
+          <p>
+            In 1982, I started a formal practice of meditation, a study of
+            Vedanta Philosophy and a Hatha Yoga practice. I taught my first
+            Hatha Yoga class in 1985 at Seton Hall University.
+          </p>
 
-          <a
-            href="https://yogawithpaz.com/"
-            className={styles.card}
-            target="_new"
-          >
-            <h3>About</h3>
-            <p>
-              In 1986 I moved to the Washington DC area and attended regular
-              hatha yoga classes. I became a participant at The Self Revelation
-              Church of Absolute Monism. The weekly service and programs
-              highlighted Advaita Vedanta, Kriya Yoga and Universality of Soul.
-              I practiced Karma Yoga working on the grounds and was a member of
-              the Bell Choir.
-            </p>
-          </a>
+          <h3>The Middle Years</h3>
+          <p>
+            In 1986 I moved to the Washington DC area and attended regular hatha
+            yoga classes. I became a participant at The Self Revelation Church
+            of Absolute Monism. The weekly service and programs highlighted
+            Advaita Vedanta, Kriya Yoga and Universality of Soul. I practiced
+            Karma Yoga working on the grounds and was a member of the Bell
+            Choir.
+          </p>
 
-          <a
-            href="https://www.ramdass.org/"
-            className={styles.card}
-            target="_new"
-          >
-            <h3>About</h3>
-            <p>
-              In 2015, I moved back to New Jersey and discovered many yoga
-              studios close to home. I completed the 200 Hour Yoga Teacher
-              Training program 2017 and immediately began teaching. My teaching
-              and practice interests are varied; I enjoy working with beginning
-              students and those with special needs. I am inspired by the
-              writings of Swami Kamalananda ("Srimati Kamala"), Swami
-              Premananda, Swami Satchidananda, Ram Dass, BKS Iyengar, and Pema
-              Chodron.
-            </p>
-          </a>
+          <h3>Be Here Now</h3>
+          <p>
+            In 2015, I moved back to New Jersey and discovered many yoga studios
+            close to home. I completed the 200 Hour Yoga Teacher Training
+            program 2017 and immediately began teaching. My teaching and
+            practice interests are varied; I enjoy working with beginning
+            students and those with special needs. I am inspired by the writings
+            of Swami Kamalananda ("Srimati Kamala"), Swami Premananda, Swami
+            Satchidananda, Ram Dass, BKS Iyengar, and Pema Chodron.
+          </p>
         </div>
       </main>
 
